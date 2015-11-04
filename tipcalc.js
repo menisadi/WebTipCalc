ad560813dbe3d34c6e3fbac2c8a8c336071ef03d
menisadi <menitm@gmail.com>
2015-11-04 22:40:00 +0200

    no style

--- tipcalc.js
+++ tipcalc.js
@@ -1,15 +1,4 @@
-function calc()
-{
-	var mymoney100 = qa100.value;
-	var mymoney10 = qa10.value;
-	var mymoney1 = qa1.value;        
-	var mymeal100 = qb100.value;
-	var mymeal10 = qb10.value;
-	var mymeal1 = qb1.value;
-	var mymoney = mymoney100 + mymoney10 + mymoney1
-	var mymeal = mymeal100 + mymeal10 + mymeal1
-	var tip = mymeal*0.15
-	var credit = +mymeal + +tip - +mymoney
-	document.getElementById('result').innerHTML = credit;
-	document.getElementById('tip').innerHTML = tip;
-} 
+ $('.selectpicker').selectpicker({
+          style: 'btn-info',
+          size: 4
+      });
