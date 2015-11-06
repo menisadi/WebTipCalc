96fe63b284caf9a80425e53c2f3a6446d93d0fc0
menisadi <menitm@gmail.com>
2015-11-06 11:18:57 +0200

    ff

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('input.small-number').on('keyup change', function() {
+    $('input.small-number').on('change', function() {
         var tip = $('#cost').val()*$('#tip_percentage').val()/100;
         var credit = Math.max(+$('#cost').val() + tip - +$('#cash').val(), 0);
         $('#result').text(credit);
