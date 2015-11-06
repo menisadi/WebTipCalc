ed9bab0acf15a4dd245c18311d2a25200ceb7a18
menisadi <menitm@gmail.com>
2015-11-06 11:26:43 +0200

    trying

--- app.js
+++ app.js
@@ -13,3 +13,18 @@ $(document).ready(function() {
     });
 });
 
+$( document ).delegate("#aboutPage", "pageinit", function() {
+    $('input.small-number').on('change', function() {
+        var tip = $('#cost').val()*$('#tip_percentage').val()/100;
+        var credit = Math.max(+$('#cost').val() + tip - +$('#cash').val(), 0);
+        $('#result').text(credit);
+        $('#tip').text("the tip is: " + tip);
+    });
+    $('select.selectpicker').on('select', function() {
+        var tip = $('#cost').val()*$('#tip_percentage').val()/100;
+        var credit = Math.max(+$('#cost').val() + tip - +$('#cash').val(), 0);
+        $('#result').text(credit);
+        $('#tip').text(tip);
+    });
+});
+
